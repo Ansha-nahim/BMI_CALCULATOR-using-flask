c15a59ea613b587d2ad64f089f5774dfd49262fd
Ansha <anshanaheem@gmail.com>
2025-05-16 16:59:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('shh.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    Height = float(request.form['Height'])
+    Weight = float(request.form['Weight'])
+    BMI=Weight/(Height*Height)
+    return f"your BMI {BMI}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
